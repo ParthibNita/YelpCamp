cc2f5f457aa6c874183b29fa235796f9b1b6950f
Parthib Deb <debparthib0040@gmail.com>
2025-06-29 18:37:33 +0530

    Add review model

---
+++ src/models/reviews.models.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+
+const reviewSchema = new Schema({
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Review = mongoose.model("Review", reviewSchema);
