32d7d91ac3f74aafc85b69e8024e9f2db1c452a1
Parthib Deb <debparthib0040@gmail.com>
2025-06-30 17:48:43 +0530

    Update CampgroundSchema

--- src/utils/Schemas.js
+++ src/utils/Schemas.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const CampgroundSchema = Joi.object({
   campgrounds: Joi.object({
     title: Joi.string().max(10).required(),
-    price: Joi.number().min(0).required(),
+    price: Joi.number().min(1).required(),
     image: Joi.string().required(),
     location: Joi.string().required(),
     description: Joi.string().allow(""),
