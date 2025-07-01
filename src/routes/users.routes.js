203bcde93863bd721fce70ed60e6ff1f732ec57d
Parthib Deb <debparthib0040@gmail.com>
2025-07-02 03:01:31 +0530

    Add register route and functionality

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.models.js";
@@ -8,5 +7,27 @@ const router = Router();
 router.route("/register").get((_, res) => {
   res.render("users/register");
 });
+
+router.route("/register").post(
+  asyncHandler(async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      // Create new user
+      const user = new User({
+        username,
+        email,
+      });
+      const registeredUser = await User.register(user, password);
+      req.flash(
+        "success",
+        `Hello <strong>${registeredUser.username}</strong>. Welcome to YelpCamp!`
+      );
+      res.redirect("/campgrounds");
+    } catch (error) {
+      req.flash("error", error.message);
+      res.redirect("/users/register");
+    }
+  })
+);
 
 export default router;
