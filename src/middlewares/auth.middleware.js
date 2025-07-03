705fdd21fde5f2c6f0af2ec68971c528b675316e
Parthib Deb <debparthib0040@gmail.com>
2025-07-03 21:54:48 +0530

    Add redirectRoute middleware to handle post-login redirection

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,9 +1,18 @@
 const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
     req.flash("error", "Please log in first");
     return res.redirect("/users/login");
   }
   next();
 };
 
-export { isLoggedIn };
+// Middleware to store redirect URL as recent passport.js version deletes the session after succesful login
+const redirectRoute = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
+  }
+  next();
+};
+
+export { isLoggedIn, redirectRoute };
