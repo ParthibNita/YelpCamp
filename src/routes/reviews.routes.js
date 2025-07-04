64f0b5171e3355f8a95cd514a48a080bb73c90a8
Parthib Deb <debparthib0040@gmail.com>
2025-07-05 00:49:12 +0530

    Add review author validation to prevent self-reviews

--- src/routes/reviews.routes.js
+++ src/routes/reviews.routes.js
@@ -4,7 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { Campground } from "../models/campground.models.js";
 import { Review } from "../models/reviews.models.js";
 import { validateReview } from "../middlewares/validate.middleware.js";
-import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import { isLoggedIn, isReviewAuthor } from "../middlewares/auth.middleware.js";
 
 const router = Router({ mergeParams: true });
 
@@ -17,6 +17,10 @@ router.route("/").post(
     if (!campground) {
       throw new ApiError(404, "Campground Not Found");
     }
+    if (campground.author.equals(req.user._id)) {
+      req.flash("error", "You can't review your own campground!");
+      return res.redirect(`/campgrounds/${id}`);
+    }
     const review = new Review({ ...req.body.reviews, author: req.user._id });
     campground.reviews.push(review);
     await review.save();
@@ -28,6 +32,7 @@ router.route("/").post(
 
 router.route("/:reviewId").delete(
   isLoggedIn,
+  isReviewAuthor,
   asyncHandler(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, {
