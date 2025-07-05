3e5f1e040c3c89855f656c81e022b38c8c4ae1d1
Parthib Deb <debparthib0040@gmail.com>
2025-07-05 11:07:02 +0530

    Debug middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import { Review } from "../models/reviews.models.js";
+import { Campground } from "../models/campground.models.js";
 
 const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -17,10 +19,18 @@ const redirectRoute = (req, res, next) => {
   next();
 };
 
-const isAuthor = (req, res, next) => {
+const isAuthor = async (req, res, next) => {
   const { id } = req.params;
-
-  if (!req.user._id.equals(id)) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    req.flash("error", "Invalid Campground ID");
+    return res.redirect("/campgrounds");
+  }
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash("error", "Campground doesn't exist");
+    return res.redirect("/campgrounds");
+  }
+  if (!campground.author.equals(req.user._id)) {
     req.flash("error", "You don't have permission to do that!");
     return res.redirect(`/campgrounds/${id}`);
   }
