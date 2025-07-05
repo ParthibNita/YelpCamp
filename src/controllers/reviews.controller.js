a880b13caf3c1d23f95b709e52957cffcbaf8b27
Parthib Deb <debparthib0040@gmail.com>
2025-07-05 11:29:17 +0530

    Implement review controller with post and delete functionality

---
+++ src/controllers/reviews.controller.js
@@ -0,0 +1,36 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Campground } from "../models/campground.models.js";
+import { Review } from "../models/reviews.models.js";
+
+const postReview = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    throw new ApiError(404, "Campground Not Found");
+  }
+  if (campground.author.equals(req.user._id)) {
+    req.flash("error", "You can't review your own campground!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  const review = new Review({ ...req.body.reviews, author: req.user._id });
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  req.flash("success", "Review added successfully!");
+  res.redirect(`/campgrounds/${id}`);
+});
+
+const deleteReview = asyncHandler(async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, {
+    $pull: {
+      reviews: reviewId,
+    },
+  });
+  await Review.findByIdAndDelete(reviewId);
+  req.flash("success", "Review deleted successfully!");
+  res.redirect(`/campgrounds/${id}`);
+});
+
+export { postReview, deleteReview };
