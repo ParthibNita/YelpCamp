370a712680c8b6816e3c0e1f8df2034471714018
Parthib Deb <debparthib0040@gmail.com>
2025-07-14 00:50:11 +0530

    Add image upload functionality to create and edit campground routes

--- src/routes/campgrounds.routes.js
+++ src/routes/campgrounds.routes.js
@@ -10,25 +10,33 @@ import {
   postEditCampground,
   deleteCampground,
 } from "../controllers/campgrounds.controllers.js";
-
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router
   .route("/")
   .get(getAllCampgrounds)
-  .post(isLoggedIn, validateCampground, createNewCampground);
+  .post(
+    isLoggedIn,
+    validateCampground,
+    upload.array("campgrounds[image]", 4),
+    createNewCampground
+  );
 
 router.route("/new").get(isLoggedIn, getNewCampground);
 
 router
   .route("/:id")
   .get(viewCampground)
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    upload.array("campgrounds[image]", 4),
+    postEditCampground
+  )
   .delete(isLoggedIn, isAuthor, deleteCampground);
 
 router.route("/:id/edit").get(isLoggedIn, isAuthor, getEditCampground);
-
-router
-  .route("/:id")
-  .put(isLoggedIn, isAuthor, validateCampground, postEditCampground);
 
 export default router;
